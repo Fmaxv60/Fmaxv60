99a368cd0241b04484aaed4e41fb9c2d305f2be2
Maxime <maxime.v60@orange.fr>
2024-07-26 14:05:08 +0200

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={'${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden'}>
-        <StarCanvas/>
+        <StarCanvas />
+        <Navbar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import StarBackground from "@/components/main/StarBackground";
 export default function Home() {
   return (
     <main className="h-ful w-full">
-      <div className="flex flex-col h-[850px] gap-20">
+      <div className="flex flex-col gap-20">
         <Hero />
       </div>
     </main>
